f48e529c73cfa491260975561f81510e0da577ef
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-05-07 19:59:47 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat [085713964963]
-│ • Gopay [085713964963]
+│ • Indosat [082119094904]
+│ • Hasil donasi akan dibelikan RDP/VPS agar bot berjalan 24 jam 
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281390658325
+│ > Ingin donasi? Wa.me/6282117332347
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
