081529bcd50031da723d6858e2de70047015e9e8
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-05-11 20:23:31 +0700

    Update anime-megumin.js

--- plugins/anime-megumin.js
+++ plugins/anime-megumin.js
@@ -1,10 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch('https://api.waifu.pics/sfw/megumin')
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
-  if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+  if (!json.url) throw m.reply(eror)
+  conn.sendBI(m.chat, `Nihh meguminnya @${m.sender.split('@')[0]}`, wm, json.url, [[`Next`, `${usedPrefix}${command}`]], m, {mention: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer() })
+  
 }
 handler.help = ['megumin']
 handler.tags = ['anime']
