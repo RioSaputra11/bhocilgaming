4e416dda72bd9a24b4231d5a72d376534fdc89ef
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-05-11 20:07:50 +0700

    Update anime-husbu.js

--- plugins/anime-husbu.js
+++ plugins/anime-husbu.js
@@ -1,12 +1,10 @@
-let fetch = require('node-fetch')
-
 let handler = async (m, { conn, usedPrefix, command }) => {
 let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/husbu.txt')
 let txt = await res.text()
 
 let arr = txt.split('\n')
 let cita = arr[Math.floor(Math.random() * arr.length)]
-  await conn.sendFile(m.chat, cita, 'husbu.jpg', '*Nihh husbunya🗿*', m)
+  await conn.sendBI(m.chat, `Nihh husbunya @${m.sender.split('@')[0]}`, wm, cita, [[`Next`, `${usedPrefix}${command}`]], m, {mentions: [m.sender], jpegThumbnail: await(await fetch(cita)).buffer()})
 }
 handler.tags = ['anime']
 handler.help = ['husbu']
